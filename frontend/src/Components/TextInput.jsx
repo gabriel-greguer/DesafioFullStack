01cb8f3fc7d173e25b8deefb58daf056c7bf3d72
Gabriel Greguer <gabriel.greguer@lasse.ind.br>
2022-08-15 12:35:11 -0300

    new front logic

--- frontend/src/Components/TextInput.jsx
+++ frontend/src/Components/TextInput.jsx
@@ -4,12 +4,37 @@ import Modal from 'react-bootstrap/Modal';
 import {MdOutlineAddTask} from 'react-icons/md'
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import axios from 'axios';
 
 const TextInput = () => {
   const [show, setShow] = useState(false);
+  const [verification,setVerification] = useState(false);
+
+  //estados do formulario
+  const [taskName,setTaskName] = useState('');
+  const [taskDuration,setTaskDuration] = useState('');
+  const [taskDescription,setTaskDescription] = useState('');
+  const [taskDate,setTaskDate] = useState('');
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const valideCreate = () => {
+    if(taskDuration && taskName && taskDescription && taskDate)
+    {
+    console.log('validado');
+    setVerification(false);
+    axios.post('http://127.0.0.1:3000/task',
+    {title:taskName,
+    duration:taskDuration,
+    description:taskDescription,
+    date:taskDate}).then(console.log('sended'))
+    }
+    else
+    {
+      setVerification(true);
+    }
+
+  }
 
   return (
     <>
@@ -22,22 +47,23 @@ const TextInput = () => {
           <Modal.Title>Criar Nova Tarefa</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <FloatingLabel controlId="floatingInput" label="Nome da tarefa"
+        <FloatingLabel onChange={(e) =>{setTaskName(e.target.value)}} controlId="floatingInput" label="Nome da tarefa"
         className="mb-3">
         <Form.Control type="text" placeholder="nome da tarefa" />
       </FloatingLabel>
-      <FloatingLabel controlId="floatingTextarea2" label="Descrição">
+      <FloatingLabel onChange={(e) => setTaskDescription(e.target.value)} controlId="floatingTextarea2" label="Descrição">
         <Form.Control
           as="textarea"
           placeholder="Leave a comment here"
           className="mb-4 h-100"
         />
       </FloatingLabel>
-      <input type="datetime-local" />
+      <input onChange={(e)=>{setTaskDate(e.target.value)}} type="datetime-local" />
+      <span style={{marginLeft:'20px'}}>data e hora da tarefa.</span>
  
 
-      <Form.Select className='mt-4' aria-label="Floating label select example">
-        <option disabled>Duração</option>
+      <Form.Select onChange={(e) => setTaskDuration(e.target.value)} className='mt-4' aria-label="Floating label select example">
+        <option selected disabled>Duração</option>
         <option value="1-hr">1 hora</option>
         <option value="2-hr">2 horas</option>
         <option value="3-hr">3 horas</option>
@@ -47,12 +73,14 @@ const TextInput = () => {
 
         </Modal.Body>
         <Modal.Footer>
+        {verification && <div>Preencha todos os campos.</div>}
           <Button variant="secondary" onClick={handleClose}>
             Fechar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={valideCreate}>
             Criar Evento
           </Button>
+          
         </Modal.Footer>
       </Modal>
     </>
